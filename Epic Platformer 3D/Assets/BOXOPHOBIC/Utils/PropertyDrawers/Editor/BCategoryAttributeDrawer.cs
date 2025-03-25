29d7facb65f7e064b7553c532d12dcf8a607d20a
Eduardo Seitz <eduardoseitz@outlook.com.br>
2025-03-25 07:44:51 -0300

    Importado skybox

---
+++ Epic Platformer 3D/Assets/BOXOPHOBIC/Utils/PropertyDrawers/Editor/BCategoryAttributeDrawer.cs
@@ -0,0 +1,33 @@
+﻿// Cristian Pop - https://boxophobic.com/
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Boxophobic
+{
+
+    [CustomPropertyDrawer(typeof(BCategoryAttribute))]
+    public class BCategoryAttributeDrawer : PropertyDrawer
+    {
+        BCategoryAttribute a;
+        private string category;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+
+            a = (BCategoryAttribute)attribute;
+            category = a.Category;
+
+            GUI.enabled = true;
+            BEditorGUI.DrawCategory(position, category);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+
+            return 40;
+
+        }
+    }
+
+}
